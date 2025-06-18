c99711211bc807507ab627d5f03300139cddb952
Fabrizio <u201914181@upc.edu.pe>
2025-06-18 18:42:24 -0500

    feat(quote): add event type value object

---
+++ src/main/java/com/eventify/platfom/planning/domain/model/valueobjects/EventType.java
@@ -0,0 +1,12 @@
+package com.eventify.platfom.planning.domain.model.valueobjects;
+
+/**
+ * Enumeration representing the type of event that organizer will design.
+ */
+
+public enum EventType{
+    CONFERENCE,
+    BIRTHDAY,
+    WEDDING,
+    GRADUATION
+}
