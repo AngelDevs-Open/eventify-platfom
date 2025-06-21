fd93c4dc6fd0b149eccb0ce8631fde43b35903c9
Fabrizio <u201914181@upc.edu.pe>
2025-06-21 16:21:52 -0500

    chore: add enable jpa auditing decorator

--- src/main/java/com/eventify/platfom/EventifyPlatfomApplication.java
+++ src/main/java/com/eventify/platfom/EventifyPlatfomApplication.java
@@ -2,8 +2,10 @@ package com.eventify.platfom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EventifyPlatfomApplication {
 
     public static void main(String[] args) {
