43014d6ebfcaa4c91e27ae1ea682718e87ccd116
JulyP <u20211d760@upc.edu.pe>
2025-06-22 14:11:23 -0500

    feat(value-object): Add SocialEventTitle value object.

--- src/main/java/com/eventify/platfom/SocialEvent/domain/model/valueobjects/SocialEventTitle.java
+++ src/main/java/com/eventify/platfom/SocialEvent/domain/model/valueobjects/SocialEventTitle.java
@@ -1,4 +1,21 @@
 package com.eventify.platfom.SocialEvent.domain.model.valueobjects;
 
-public record SocialEventTitle() {
+
+public record SocialEventTitle(String title) {
+
+
+    /**
+     * Constructor for SocialEventTitle.
+     *
+     * @param title the title of the social event
+     * @throws IllegalArgumentException if the socialEventTitle is null or empty
+     */
+    public SocialEventTitle {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException("Social Event Title cannot be null or empty");
+        }
+    }
+
+
+
 }
