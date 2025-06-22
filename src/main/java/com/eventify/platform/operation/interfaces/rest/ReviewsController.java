5692db859621ad923a1a8540e353895ec76d433f
OmBRz <omarbrrcl@gmail.com>
2025-06-21 23:12:02 -0500

    feat(review): add reviews controller.

--- src/main/java/com/eventify/platform/operation/interfaces/rest/ReviewsController.java
+++ src/main/java/com/eventify/platform/operation/interfaces/rest/ReviewsController.java
@@ -1,18 +1,22 @@
 package com.eventify.platform.operation.interfaces.rest;
 
+import com.eventify.platform.operation.domain.model.queries.GetAllReviewsQuery;
+import com.eventify.platform.operation.domain.model.queries.GetReviewByIdQuery;
 import com.eventify.platform.operation.domain.services.ReviewCommandService;
 import com.eventify.platform.operation.domain.services.ReviewQueryService;
 import com.eventify.platform.operation.interfaces.rest.resources.CreateReviewResource;
 import com.eventify.platform.operation.interfaces.rest.resources.ReviewResource;
 import com.eventify.platform.operation.interfaces.rest.transform.CreateReviewCommandFromResourceAssembler;
+import com.eventify.platform.operation.interfaces.rest.transform.ReviewResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -37,6 +41,41 @@ public class ReviewsController {
     public ResponseEntity<ReviewResource> createReview(CreateReviewResource resource) {
         var createReviewCommand = CreateReviewCommandFromResourceAssembler.toCommandFromResource(resource);
         var reviewId = reviewCommandService.handle(createReviewCommand);
-        return null; // Ideally, you would set the URI of the created resou
+        if( reviewId == null || reviewId == 0L) return ResponseEntity.badRequest().build();
+        var getReviewByIdQuery = new GetReviewByIdQuery(reviewId);
+        var review = reviewQueryService.handle(getReviewByIdQuery);
+        if (review.isEmpty()) return ResponseEntity.notFound().build();
+        var reviewEntity = review.get();
+        var reviewResource = ReviewResourceFromEntityAssembler.toResourceFromEntity(reviewEntity);
+        return new ResponseEntity<>(reviewResource, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{reviewId}")
+    @Operation(summary = "Get review by id", description = "Get review by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Review found"),
+            @ApiResponse(responseCode = "404", description = "Review not found")})
+    public ResponseEntity<ReviewResource> getReviewById(@PathVariable Long reviewId){
+        var getReviewByIdQuery = new GetReviewByIdQuery(reviewId);
+        var review = reviewQueryService.handle(getReviewByIdQuery);
+        if (review.isEmpty()) return ResponseEntity.notFound().build();
+        var reviewEntity = review.get();
+        var reviewResource = ReviewResourceFromEntityAssembler.toResourceFromEntity(reviewEntity);
+        return ResponseEntity.ok(reviewResource);
+    }
+
+    @GetMapping
+    @Operation(summary = "Get all reviews", description = "Get all reviews")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Reviews found"),
+            @ApiResponse(responseCode = "404", description = "No reviews found")})
+    public ResponseEntity<List<ReviewResource>> getAllReviews(){
+        var reviews = reviewQueryService.handle(new GetAllReviewsQuery());
+        if (reviews.isEmpty()) return ResponseEntity.notFound().build();
+        var reviewResources = reviews.stream()
+                .map(ReviewResourceFromEntityAssembler::toResourceFromEntity)
+                .toList();
+        return ResponseEntity.ok(reviewResources);
+    }
+
 }
