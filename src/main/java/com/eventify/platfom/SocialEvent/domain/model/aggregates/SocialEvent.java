9f976536fa34630c338a712eaf99f91df745c4f8
JulyP <u20211d760@upc.edu.pe>
2025-06-22 14:16:20 -0500

    feat(aggregate): Add SocialEvent.

--- src/main/java/com/eventify/platfom/SocialEvent/domain/model/aggregates/SocialEvent.java
+++ src/main/java/com/eventify/platfom/SocialEvent/domain/model/aggregates/SocialEvent.java
@@ -1,4 +1,107 @@
 package com.eventify.platfom.SocialEvent.domain.model.aggregates;
 
-public class SocialEvent {
+
+
+import com.eventify.platfom.SocialEvent.domain.model.valueobjects.*;
+import com.eventify.platfom.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+
+/*
+*SocialEvent Aggregate Root
+*
+*/
+@Entity
+public class SocialEvent extends AuditableAbstractAggregateRoot<SocialEvent>
+{
+
+    @Embedded
+    private SocialEventTitle title;
+
+    @Embedded
+    private CustomerName customerName;
+
+    @Embedded
+    private Place place;
+
+    @Embedded
+    private SocialEventDate date;
+
+    @Embedded
+    private SocialEventStatus status;
+
+
+
+
+
+
+    /**
+     * Constructor with full value objects.
+     *
+     * @param title        the title of the event
+     * @param place        the location of the event
+     * @param date         the scheduled date
+     * @param customerName the customer full name
+     * @param status       the current status of the event
+     */
+    public SocialEvent(SocialEventTitle title, Place place, SocialEventDate date,
+                       CustomerName customerName, SocialEventStatus status) {
+        this.title = title;
+        this.place = place;
+        this.date = date;
+        this.customerName = customerName;
+        this.status = status;
+
+    }
+
+    protected SocialEvent() {}
+
+    // Getters for each embedded value object
+
+    public String getTitle() {
+        return title.title();
+    }
+
+    public String getPlace() {
+        return place.place();
+    }
+
+    public String getCustomerName() {
+        return customerName.customerName();
+    }
+
+    public java.time.LocalDate getDate() {
+        return date.eventDate();
+    }
+
+    public String getEventStatus() {
+        return status.valueStatus();
+    }
+
+
+    /**
+     * Allows updating the event's status.
+     *
+     * @param newStatus the new status to set
+     */
+    public void updateStatus(String newStatus) {
+        this.status = new SocialEventStatus(newStatus);
+
+
+    }
+
+    /**
+     * Allows updating basic event details.
+     *
+     * @param newTitle new title
+     * @param newPlace new place
+     * @param newDate  new date
+     */
+    public void updateDetails(String newTitle, String newPlace, java.time.LocalDate newDate) {
+        this.title = new SocialEventTitle(newTitle);
+        this.place = new Place(newPlace);
+        this.date = new SocialEventDate(newDate);
+    }
 }
+
+
+
