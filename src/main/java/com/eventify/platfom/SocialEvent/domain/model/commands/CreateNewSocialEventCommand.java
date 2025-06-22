32a9eda84be7b4def540a7bf0c68251506324ece
JulyP <u20211d760@upc.edu.pe>
2025-06-22 14:17:53 -0500

    feat(command): Add Command to create new social event.

--- src/main/java/com/eventify/platfom/SocialEvent/domain/model/commands/CreateNewSocialEventCommand.java
+++ src/main/java/com/eventify/platfom/SocialEvent/domain/model/commands/CreateNewSocialEventCommand.java
@@ -1,4 +1,20 @@
 package com.eventify.platfom.SocialEvent.domain.model.commands;
 
-public record CreateNewSocialEventCommand() {
+/**
+ * Command to request the creation of a Social Event.
+ *
+ * @param title        the title of the event
+ * @param place        the location of the event
+ * @param date         the date the event will take place
+ * @param customerName the name of the event organizer
+ * @param status       the initial status (e.g., ACTIVE, TO_CONFIRM)
+ */
+
+public record CreateNewSocialEventCommand(
+    String title,
+    String place,
+    String date,
+    String customerName,
+    String status
+) {
 }
