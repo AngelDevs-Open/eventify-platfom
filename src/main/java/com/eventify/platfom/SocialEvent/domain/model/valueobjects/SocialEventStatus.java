851334e4402f51e76154fa8ecebe72e38d933312
JulyP <u20211d760@upc.edu.pe>
2025-06-22 14:11:04 -0500

    feat(value-object): Add SocialEventStatus value object.

--- src/main/java/com/eventify/platfom/SocialEvent/domain/model/valueobjects/SocialEventStatus.java
+++ src/main/java/com/eventify/platfom/SocialEvent/domain/model/valueobjects/SocialEventStatus.java
@@ -1,4 +1,17 @@
 package com.eventify.platfom.SocialEvent.domain.model.valueobjects;
 
-public record SocialEventStatus() {
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.*;
+
+
+public record SocialEventStatus(String valueStatus) {
+
+    public SocialEventStatus {
+        if (valueStatus == null) {
+            throw new IllegalArgumentException("SocialEventStatus cannot be null ");
+        }
+
+    }
+
 }
