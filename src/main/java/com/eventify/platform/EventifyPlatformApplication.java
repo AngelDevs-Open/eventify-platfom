77207bd87330fc78f42c2ccff97c3d4be3de80db
JulyP <u20211d760@upc.edu.pe>
2025-07-07 18:17:36 -0500

    fix(application): correct spelling of eventify platform application and enable jpa auditing.

--- src/main/java/com/eventify/platform/EventifyPlatformApplication.java
+++ src/main/java/com/eventify/platform/EventifyPlatformApplication.java
@@ -2,12 +2,14 @@ package com.eventify.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class EventifyPlatfomApplication {
+@EnableJpaAuditing
+public class EventifyPlatformApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EventifyPlatfomApplication.class, args);
+        SpringApplication.run(EventifyPlatformApplication.class, args);
     }
 
 }
