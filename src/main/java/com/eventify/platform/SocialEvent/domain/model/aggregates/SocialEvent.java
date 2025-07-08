2e986dc447b55445d9e481f1a36bb6e3410ec369
Fabrizio <u201914181@upc.edu.pe>
2025-07-07 21:16:07 -0500

    chore: delete unnecesary files

--- src/main/java/com/eventify/platform/SocialEvent/domain/model/aggregates/SocialEvent.java
+++
@@ -1,4 +0,0 @@
-package com.eventify.platform.SocialEvent.domain.model.aggregates;
-
-public class SocialEvent {
-}
--- src/main/java/com/eventify/platform/SocialEvent/domain/model/commands/OrderSocialEvent.java
+++
@@ -1,4 +0,0 @@
-package com.eventify.platform.SocialEvent.domain.model.commands;
-
-public record OrderSocialEvent() {
-}
