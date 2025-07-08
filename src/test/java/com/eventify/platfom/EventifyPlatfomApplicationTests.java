92aafed4316d5ec9a8d97b04e99a4171ca3060a7
Fabrizio <u201914181@upc.edu.pe>
2025-07-07 21:09:57 -0500

    chore: add EventifyPlatfomApplicationTests for avoid conflict in event-management branch

---
+++ src/test/java/com/eventify/platfom/EventifyPlatfomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventify.platfom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventifyPlatfomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
